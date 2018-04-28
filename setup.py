432de0aa52f5b601478c5ea06e0e72403a4ae113
Govind <govindnh4cl@gmail.com>
2018-04-28 15:00:20 -0400

    Add test.csv as data_files in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='Mytest',
-   version='1.0',
-   description='A useful module',
-   author='Govind',
-   author_email='example@example.com',
-   packages=['mytest'],  #same as name
-   # install_requires=['bar', 'greek'], #external packages as dependencies
+    name='Mytest',
+    version='1.0',
+    description='A useful module',
+    author='Govind',
+    author_email='example@example.com',
+    packages=['mytest'],  #same as name
+    data_files=[('mytest', ['mytest/test.csv'])]   
 )
