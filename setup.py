e291ac060606be238fafd65ad31d30a285284338
dromosys <android.robot.factory@gmail.com>
2018-07-17 13:21:13 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='mytest',
+    name='dromosystest',
     version='1.1',
     description='A useful module',
     author='Govind',
     author_email='example@example.com',
-    packages=['mytest'],  #same as name
+    packages=['dromosystest'],  #same as name
 )
